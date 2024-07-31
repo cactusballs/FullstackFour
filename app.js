d68f4e8e211703cebced02ec76f90f3b715a6aab
Buzz Lightyear <cyscodelario@gmail.com>
2024-07-31 17:19:00 +0100

    Add Cynthia's hobby

--- app.js
+++ app.js
@@ -7,3 +7,9 @@ let lottie = {
 
 console.log(`My name's ${lottie.fullName} and I like ${lottie.hobby}!`);
 
+let cynthia = {
+    fullName: 'Cynthia Scodelario-Brennan',
+    hobby: 'travelling'
+}
+
+console.log(`My name's ${cynthia.fullName} and I like ${cynthia.hobby}!`);
