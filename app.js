30d961398c85c6d4049ae031af64b54c871f95c1
Cat C <usertester094@gmail.com>
2024-07-31 17:26:13 +0100

    added cat's name and hobby

--- app.js
+++ app.js
@@ -13,3 +13,12 @@ let cynthia = {
 }
 
 console.log(`My name's ${cynthia.fullName} and I like ${cynthia.hobby}!`);
+
+
+
+let cat = {
+    fullName: 'Cat Conquest',
+    hobby: 'Yoga'
+}
+
+console.log(`My name's ${cat.fullName} and I like ${cat.hobby}!`);
