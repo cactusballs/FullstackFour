2947a2286a3ca231364a3fe0f08a24b5415562eb
Buzz Lightyear <cyscodelario@gmail.com>
2024-08-06 18:26:04 +0100

    Adding first version of login page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,43 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from './components/login/Login';
+import './App.css';
 
+// Main App component with routing
 function App() {
-  const [count, setCount] = useState(0)
-
-  const namesHobbies = [{name: "Anh", message: "I like sharing cat memes"}]
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <div>
-        {namesHobbies.map((item) => {
-        return (<>
-          <h4>{item.name}</h4>
-          <p>{item.message}</p>
-        </>)
-      })}
-      </div>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
