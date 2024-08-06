992574ad109442b14451bc44970e1427ed167c24
Anh <97702902+hihaianh@users.noreply.github.com>
2024-08-06 23:05:28 +0100

    starting broadcast component
    frontend + backend

--- server/app.js
+++ server/app.js
@@ -1,45 +1,63 @@
-// creating the server 
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
 const app = express();
-const sql = require('mysql2');
-require('dotenv').config();
-
-app.use(express.json());
-
-// connecting to the DB
-const database = sql.createConnection({
+const mysql = require("mysql2/promise");
+const database = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
 });
 
-// checking we're connected to the DataBase 
-database.connect((error) => {
- if(error){
-  console.error('Error connecting to the database:', error.stack);
-  return;
- }
- console.log('Connected to the database.');
-});
+app.use(cors());
+app.use(express.json());
 
 // creating route end point
 app.get(`/`, (req, res) => {
-  res.status(200).send('Welcome to your Village!')});
+  res.status(200).send("Welcome to your Village!");
+});
 
-// getting all villagers
-app.get('/villagers', (req, res) => {
-  const userList = 'SELECT * FROM villagers ORDER BY villager_id ASC'
-  database.query(userList, (error, results) => {
-    if (error) {
-      return res.status(500).json({ message: 'An error has occurred', error: error.message });
-    }
+// getting messages
+app.get("/broadcastmessages", async (req, res) => {
+  const query = `SELECT v.user_name, b.message_content 
+  FROM broadcast_messages b 
+  JOIN villagers v 
+  ON b.villager_id = v.villager_id 
+  WHERE TIMESTAMPDIFF(HOUR, b.created_at, NOW()) < 24
+  ORDER BY created_at ASC;`;
+
+  try {
+    const [results] = await database.query(query);
     res.status(200).json(results);
-  });
-})
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({ status: "Failed to retrieve messages" });
+  }
+});
 
-// creating and connecting to the port 
+// users create + post messages
+app.post("/broadcastmessages", async (req, res) => {
+  const { id, message_content } = req.body;
+
+  if (!message_content || !id) {
+    res.status(400).json({ status: "Values cannot be blank" });
+  }
+
+  try {
+    await database.query(
+      "INSERT INTO broadcast_messages (villager_id, message_content) VALUES (?, ?)",
+      [id, message_content]
+    );
+    res.status(201).json({ status: "Message posted", data: req.body });
+  } catch (err) {
+    res.status(400).json({ status: "Unable to post message" });
+  }
+});
+
+// creating and connecting to the port
 const port = 3000;
+
 app.listen(port, () => {
   console.log(`server is running on http://localhost:${port}`);
 });
