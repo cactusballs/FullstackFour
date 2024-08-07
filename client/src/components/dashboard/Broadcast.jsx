129bd2a2cd3fc1aa40e627fee6f1efe6c87c620a
Anh <97702902+hihaianh@users.noreply.github.com>
2024-08-07 09:05:48 +0100

    Carousel functionality

--- client/src/components/dashboard/Broadcast.jsx
+++ client/src/components/dashboard/Broadcast.jsx
@@ -1,10 +1,46 @@
 import "./Broadcast.css";
 import React from "react";
 import { useState, useEffect } from "react";
+import { CaretLeftBold } from "../icons/CaretLeft.jsx";
+import { CaretRightBold } from "../icons/CaretRight.jsx";
 
 function BroadcastMessages() {
   const [messages, setMessages] = useState([]);
+  const [currentIndex, setCurrentIndex] = useState(0);
+  // chaining
+  const currentMessage = messages?.[currentIndex];
+  const displayMessage = currentMessage && (
+    <>
+      <div className="broadcast-item">
+        <p className="broadcast-author">{currentMessage.user_name}</p>
+        <p className="broadcast-message">{currentMessage.message_content}</p>
+      </div>
+    </>
+  );
 
+  const previousMessage = () => {
+    const maxLength = messages.length - 1;
+    // when currentIndex is below 0, loop back to the end
+    if (currentIndex - 1 < 0) {
+      setCurrentIndex(maxLength);
+    } else {
+      setCurrentIndex(currentIndex - 1);
+    }
+  };
+
+  const nextMessage = () => {
+    const maxLength = messages.length - 1;
+    // until it reaches the end of messages, loop back to the start
+    if (currentIndex + 1 > maxLength) {
+      setCurrentIndex(0);
+    } else {
+      setCurrentIndex(currentIndex + 1);
+    }
+  };
+
+  const emptyCarousel = <p>Currently no new broadcast messages</p>;
+
+  // calling endpoint from frontend
   useEffect(() => {
     async function fetchMessages() {
       try {
@@ -26,7 +62,7 @@ function BroadcastMessages() {
       }
     }
     fetchMessages();
-  });
+  }, []);
 
   return (
     <>
@@ -57,23 +93,18 @@ function BroadcastMessages() {
           </svg>
         </div>
         <div className="dashboard-card-content">
-          {/* {messages.map((message, index) => {
-
-          
-            return (
-              <>
-                <div className="broadcast-item">
-                  <p className="broadcast-author">{message.user_name}</p>
-                  <p className="broadcast-message" key={index}>
-                    {message.message_content}
-                  </p>
-                </div>
-              </>
-            );
-          })} */}
+          {/* if there are no messages, display displayCarousel message else display current index */}
+          {!messages.length ? emptyCarousel : displayMessage}
         </div>
         <div className="dashboard-card-footer">
-          <div className="broadcast-arrows"></div>
+          <div className="broadcast-arrows">
+            <button onClick={previousMessage}>
+              <CaretLeftBold />
+            </button>
+            <button onClick={nextMessage}>
+              <CaretRightBold />
+            </button>
+          </div>
           <button className="create-message">Create</button>
         </div>
       </div>
