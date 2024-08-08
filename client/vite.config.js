0e623199cb289e707d48e0eaa1c7156fde2757c2
Buzz Lightyear <cyscodelario@gmail.com>
2024-08-08 06:52:37 +0100

    modify vite.config.js to speficy port number

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    open: true // to enable auto-opening of browser
+    port: 3000,
   }
 })
