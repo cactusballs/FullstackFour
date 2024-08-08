22c599f5615c61b6c909a5f6798f9105be52f3e3
cactusballs <71734928+cactusballs@users.noreply.github.com>
2024-08-08 08:35:03 +0100

    starting nav bar

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,1 +1,35 @@
-// TODO: add code for navbar  
+
+import React from 'react';
+import './Navbar.css';
+
+const Navbar = () => {
+  return (
+<nav className="navbar">
+  <div className='navbar-left'>
+    <a href='/'>Village</a>
+  </div>
+
+  <div className="navbar-center">
+    <ul className="nav-links">
+      <li>
+        <a href="/Events">Events</a>
+      </li>
+      <li>
+        <a href="/Forum">Forum</a>
+      </li>
+      <li>
+        <a href="/AboutUs">About Us</a>
+      </li>
+    </ul>
+
+    <div className="navbar-right">
+    <a href="/account" className="user-icon">
+      <i className="fas fa-user"></i>
+    </a>
+  </div>
+  </div>
+</nav>
+);
+};
+
+export default Navbar;
