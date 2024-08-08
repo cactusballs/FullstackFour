b8cd5ccd92b9c7463b840955df0e58a7979e614b
Anh <97702902+hihaianh@users.noreply.github.com>
2024-08-08 14:23:08 +0100

    Adding broadcast endpoints to server

--- server/app.js
+++ server/app.js
@@ -1,9 +1,8 @@
-// creating the server 
-const express = require('express');
+// creating the server
+const express = require("express");
 const app = express();
-const sql = require('mysql2/promise'); //possibly don't need import here anymore? as db.js handles
-const cors = require('cors');
-require('dotenv').config({ path: '../.env' });
+const cors = require("cors");
+require("dotenv").config({ path: "../.env" });
 const database = require("./database");
 
 app.use(cors());
@@ -16,16 +15,11 @@ app.use("/threads", threadRouter);
 const topicRouter = require("./routes/topics");
 app.use("/api/topics", topicRouter);
 
-
-
-
-// creating and connecting to the port 
-const port = process.env.SERVER_PORT || 3000; ;
+// creating and connecting to the port
+const port = process.env.SERVER_PORT || 3000;
 app.listen(port, () => {
   console.log(`server is running on http://localhost:${port}`);
 });
-
-
 
 //checking connection and error-logging if not working
 app.get("/check", async (req, res) => {
@@ -36,96 +30,142 @@ app.get("/check", async (req, res) => {
     res.status(200).json({ message: successMessage });
     console.log(successMessage);
   } catch (err) {
-    const errorMessage = `Request failed, error: ${err.message}`
+    const errorMessage = `Request failed, error: ${err.message}`;
     res.status(500).json({ message: errorMessage });
     console.log(errorMessage);
   }
 });
 
-
 // creating route end point
 app.get(`/`, (req, res) => {
-  res.status(200).send('Welcome to your Village!')});
-
-
-
-
-
+  res.status(200).send("Welcome to your Village!");
+});
 
 // getting all villagers
-app.get('/villagers', async (req, res) => {
-  const userList = 'SELECT * FROM villagers ORDER BY villager_id ASC';
+app.get("/villagers", async (req, res) => {
+  const userList = "SELECT * FROM villagers ORDER BY villager_id ASC";
   try {
     const [results] = await database.query(userList);
     res.status(200).json(results);
   } catch (error) {
-    res.status(500).json({ message: 'An error has occurred', error: error.message });
+    res
+      .status(500)
+      .json({ message: "An error has occurred", error: error.message });
   }
 });
-
-
-
 
 // getting thread 1
 //not a final idea for coding but shows how the tables come together to generate threads and posts.
 //final FE will need thread title at the top but this can't be shown as part of 'union' query.
 
-app.get('/thread1', async (req, res) => {
-  const thread1 = 'SELECT thread_id, user_name, content, sent_at, \'thread\' AS level '+
-  'FROM threads UNION SELECT thread_id, user_name, content, sent_at, \'post\' AS level '+
-  'FROM posts_to_threads ORDER BY thread_id, sent_at;'
+app.get("/thread1", async (req, res) => {
+  const thread1 =
+    "SELECT thread_id, user_name, content, sent_at, 'thread' AS level " +
+    "FROM threads UNION SELECT thread_id, user_name, content, sent_at, 'post' AS level " +
+    "FROM posts_to_threads ORDER BY thread_id, sent_at;";
 
   try {
     const [results] = await database.query(thread1);
     res.status(200).json(results);
   } catch (error) {
-    res.status(500).json({ message: 'An error has occurred', error: error.message });
+    res
+      .status(500)
+      .json({ message: "An error has occurred", error: error.message });
   }
 });
 
-
-
-
-
 // Route to create a new forum topic
-app.post('/api/forum', async (req, res) => {
-  const { title, content, user_name, topic, carers_tag, expecting_parents_tag, new_parents_tag, single_parents_tag, LGBTQIA_plus_parents_tag } = req.body;
+app.post("/api/forum", async (req, res) => {
+  const {
+    title,
+    content,
+    user_name,
+    topic,
+    carers_tag,
+    expecting_parents_tag,
+    new_parents_tag,
+    single_parents_tag,
+    LGBTQIA_plus_parents_tag,
+  } = req.body;
   const sql = `
     INSERT INTO threads (thread_title, content, user_name, topic, carers_tag, expecting_parents_tag, new_parents_tag, single_parents_tag, LGBTQIA_plus_parents_tag)
     VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
   `;
-  const values = [title, content, user_name, topic, carers_tag, expecting_parents_tag, new_parents_tag, single_parents_tag, LGBTQIA_plus_parents_tag];
+  const values = [
+    title,
+    content,
+    user_name,
+    topic,
+    carers_tag,
+    expecting_parents_tag,
+    new_parents_tag,
+    single_parents_tag,
+    LGBTQIA_plus_parents_tag,
+  ];
 
   try {
     await database.query(sql, values);
-    res.status(201).json({ message: 'Thread created successfully' });
+    res.status(201).json({ message: "Thread created successfully" });
   } catch (error) {
-    res.status(500).json({ message: 'An error occurred', error: error.message });
+    res
+      .status(500)
+      .json({ message: "An error occurred", error: error.message });
   }
 });
 
-
-
-
 // Route to fetch user data
-app.get('/api/user/:userId', async (req, res) => {
+app.get("/api/user/:userId", async (req, res) => {
   const userId = req.params.userId;
-  const sql = 'SELECT * FROM villagers WHERE villager_id = ?';
+  const sql = "SELECT * FROM villagers WHERE villager_id = ?";
 
   try {
     const [results] = await database.query(sql, [userId]);
     if (results.length === 0) {
-      return res.status(404).json({ message: 'User not found' });
+      return res.status(404).json({ message: "User not found" });
     }
     res.status(200).json(results[0]);
   } catch (error) {
-    res.status(500).json({ message: 'An error occurred', error: error.message });
+    res
+      .status(500)
+      .json({ message: "An error occurred", error: error.message });
   }
 });
 
+// getting messages
+app.get("/broadcastmessages", async (req, res) => {
+  const query = `SELECT v.user_name, b.message_content 
+  FROM broadcast_messages b 
+  JOIN villagers v 
+  ON b.villager_id = v.villager_id 
+  WHERE TIMESTAMPDIFF(HOUR, b.created_at, NOW()) < 24
+  ORDER BY created_at ASC;`;
 
+  try {
+    const [results] = await database.query(query);
+    res.status(200).json(results);
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({ status: "Failed to retrieve messages" });
+  }
+});
 
+// users create + post messages
+app.post("/broadcastmessages", async (req, res) => {
+  const { id, message_content } = req.body;
 
+  if (!message_content || !id) {
+    res.status(400).json({ status: "Values cannot be blank" });
+  }
 
+  try {
+    await database.query(
+      "INSERT INTO broadcast_messages (villager_id, message_content) VALUES (?, ?)",
+      [id, message_content]
+    );
+    res.status(201).json({ status: "Message posted", data: req.body });
+  } catch (err) {
+    res.status(400).json({ status: "Unable to post message" });
+  }
+});
 
 module.exports = database;
