142eb270cae74ea2caed5476f5121ee3b917de64
Maria Amparo Lopez <m.amparo.dev@gmail.com>
2024-08-10 13:29:54 +0200

    add registration component and include in router

---
+++ client/src/components/registration/Registration.jsx
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from 'react';
+import './Registration.css';
+import logo from '../../assets/images/village-logo.png';
+
+const Registration = () => {
+    return (
+        <div className="signup-container">
+        </div>
+    );
+}
+
+export default Registration;
