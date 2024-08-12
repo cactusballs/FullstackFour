3bc4c9661e74a26f1b0146d0303f2ba1b9674cc3
Chloe Kirkbride-van Beijnen <chloe.kirkbride@hotmail.co.uk>
2024-08-12 11:39:17 +0100

    updated images in ImageCarousel.jsx

--- client/src/components/dashboard/ImageCarousel.jsx
+++ client/src/components/dashboard/ImageCarousel.jsx
@@ -5,9 +5,9 @@ import './ImageCarousel.css';
 
 const ImageCarousel = () => {
     const images = [
-        'https://via.placeholder.com/600x400.png?text=Image+1',
-        'https://via.placeholder.com/600x400.png?text=Image+2',
-        'https://via.placeholder.com/600x400.png?text=Image+3',
+        'https://images.unsplash.com/photo-1506918092809-0ba639cd1385?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        'https://images.unsplash.com/photo-1532499016263-f2c3e89de9cd?q=80&w=1780&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        'https://images.unsplash.com/photo-1470608756445-2c9906b0680f?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
     ];
 
     return (
