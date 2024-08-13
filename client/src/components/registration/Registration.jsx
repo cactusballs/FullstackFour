ca48a009af0342137a8b0350d33db79839ab4bfb
Maria Amparo Lopez <m.amparo.dev@gmail.com>
2024-08-13 19:10:21 +0200

    Formatting, adding value attribute in input fields, declaring variables with useState

--- client/src/components/registration/Registration.jsx
+++ client/src/components/registration/Registration.jsx
@@ -1,66 +1,135 @@
-import React, { useState, useEffect } from 'react';
-import './Registration.css';
-import logo from '../../assets/images/village-logo.png';
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import "./Registration.css";
+import logo from "../../assets/images/village-logo.png";
 
 const Registration = () => {
-    return (
-        <div className="signup-container">
-            <div className="signup-form">
-                <div className="logo">
-                    <img src={logo} alt="Village Logo" />
-                </div>
-                <form>
-                    <div>
-                        <label htmlFor="first_name">First Name</label>
-                        <input type="text" id="first_name" name="first_name" required />
-                    </div>
-                    <div>
-                        <label htmlFor="last_name">Last Name</label>
-                        <input type="text" id="last_name" name="last_name" required />
-                    </div>
-                    <div>
-                        <label htmlFor="username">Username</label>
-                        <input type="text" id="username" name="username" required />
-                    </div>
-                    <div>
-                        <label htmlFor="birthday">Birthday</label>
-                        <input type="date" id="birthday" name="birthday" required />
-                    </div>
-                    <div>
-                        <label htmlFor="email">Email</label>
-                        <input type="email" id="email" name="email" required />
-                    </div>
-                    <div>
-                        <label htmlFor="address">Address</label>
-                        <input type="text" id="address" name="address" placeholder="Street name and number" required />
-                    </div>
-                    <div>
-                        <label htmlFor="postcode">Postcode</label>
-                        <input type="text" id="postcode" name="postcode" required />
-                    </div>
-                    <div>
-                        <label htmlFor="location">Location</label>
-                        <select id="location" name="location">
-                            <option value="north">North London</option>
-                            <option value="south">South London</option>
-                            <option value="west">West London</option>
-                            <option value="east">East London</option>
-                        </select>
-                    </div>
-                    <div>
-                        <label htmlFor="password">Password</label>
-                        <input type="password" id="password" name="password" required />
-                    </div>
-                    <div>
-                        <label htmlFor="repeat_password">Repeat Password</label>
-                        <input type="password" id="repeat_password" name="repeat_password" required />
-                    </div>
-                    <button type="submit">Sign Up</button>
-                    <p>Already have an account? <a href="./login">Log in!</a></p>
-                </form>
-            </div>
+  const navigate = useNavigate();
+  const [first_name, setFirst_name] = useState("");
+  const [last_name, setLast_name] = useState("");
+  const [user_name, setUser_name] = useState("");
+  const [birthday, setBrithday] = useState("");
+  const [email, setEmail] = useState("");
+  const [villager_address, setVillager_address] = useState("");
+  const [villager_location, setVillager_location] = useState("");
+  const [password, setPassword] = useState("");
+  const [repeat_password, setRepeat_password] = useState("");
+
+  return (
+    <div className="signup-container">
+      <div className="signup-form">
+        <div className="logo">
+          <img src={logo} alt="Village Logo" />
         </div>
-    );
-}
+        <form>
+          <div>
+            <label htmlFor="first_name">First Name</label>
+            <input
+              type="text"
+              id="first_name"
+              name="first_name"
+              value={first_name}
+              required
+            />
+          </div>
+          <div>
+            <label htmlFor="last_name">Last Name</label>
+            <input
+              type="text"
+              id="last_name"
+              name="last_name"
+              value={last_name}
+              required
+            />
+          </div>
+          <div>
+            <label htmlFor="user_name">Username</label>
+            <input
+              type="text"
+              id="user_name"
+              name="user_name"
+              value={user_name}
+              required
+            />
+          </div>
+          <div>
+            <label htmlFor="birthday">Birthday</label>
+            <input
+              type="date"
+              id="birthday"
+              name="birthday"
+              value={birhtday}
+              required
+            />
+          </div>
+          <div>
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              value={email}
+              required
+            />
+          </div>
+          <div>
+            <label htmlFor="address">Address</label>
+            <input
+              type="text"
+              id="address"
+              name="address"
+              placeholder="Street name and number"
+              value={address}
+              required
+            />
+          </div>
+          <div>
+            <label htmlFor="postcode">Postcode</label>
+            <input
+              type="text"
+              id="postcode"
+              name="postcode"
+              value={postcode}
+              required
+            />
+          </div>
+          <div>
+            <label htmlFor="location">Location</label>
+            <select id="location" name="location">
+              <option value="North London">North London</option>
+              <option value="South London">South London</option>
+              <option value="West London">West London</option>
+              <option value="East London">East London</option>
+            </select>
+          </div>
+          <div>
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              value={postcode}
+              required
+            />
+          </div>
+          <div>
+            <label htmlFor="repeat_password">Repeat Password</label>
+            <input
+              type="password"
+              id="repeat_password"
+              name="repeat_password"
+              value={postcode}
+              required
+            />
+          </div>
+          <button type="submit">Sign Up</button>
+          <p>
+            Already have an account? <a href="./login">Log in!</a>
+          </p>
+        </form>
+      </div>
+    </div>
+  );
+};
 
 export default Registration;
