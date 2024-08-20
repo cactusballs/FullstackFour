2dc36472f7e86578151ff3a50fd37c55265be9ce
Cat C <usertester094@gmail.com>
2024-08-20 21:51:28 +0100

    fixed build issue

--- client/src/components/forum/BackButton.jsx
+++ client/src/components/forum/BackButton.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Button from "react-bootstrap/button";
+import Button from "react-bootstrap/Button";
 import { useNavigate } from "react-router-dom";
 import './BackButton.css';
 
