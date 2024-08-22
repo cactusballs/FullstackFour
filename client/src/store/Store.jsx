18db4f88a85e8b74a03c8f3ca1ba07e03688e324
Maria Amparo Lopez <m.amparo.dev@gmail.com>
2024-08-22 16:51:41 +0200

    adding store to main.jsx

--- client/src/store/Store.jsx
+++ client/src/store/Store.jsx
@@ -1,4 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
+import UserReducer from "./UserSlice";
 
 const store = configureStore({
     reducer:{
--- client/src/store/UserSlice.jsx
+++ client/src/store/UserSlice.jsx
@@ -1,9 +1,10 @@
-import { CreateSlice } from "@reduxjs/toolkit";
+import { createSlice } from "@reduxjs/toolkit";
 
 const userSlice = createSlice({
     name: "user",
     initialState:{  
-        user: null
+        user: null,
+        error: null
     }
 });
 
