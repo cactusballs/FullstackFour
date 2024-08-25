86cf077a7b96091d100c524dcaa99ffd84c5c0cd
Maria Amparo Lopez <m.amparo.dev@gmail.com>
2024-08-25 10:21:38 +0200

    minor correction

--- client/src/components/login/Login.test.js
+++ client/src/components/login/Login.test.js
@@ -3,7 +3,7 @@ import { render, screen } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import configureStore from 'redux-mock-store';
 import { BrowserRouter as Router } from 'react-router-dom';
-import Login from './Login';
+import Login from './Login.jsx';
 
 const mockStore = configureStore([]);
 
