4305556f6df1e0cbdc9460784f6c4c2885157a21
Cat C <usertester094@gmail.com>
2024-08-25 19:08:36 +0100

    css tweaks

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,14 +2,11 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 import ForumTopicThreads from "./components/forum/ForumTopicThreads";
-import RecentForumPosts from "./components/dashboard/RecentForumPosts";
-import Poll from "./components/dashboard/Poll";
 import ForumMain from "./components/forum/ForumMain";
 import Login from "./components/login/Login"; // Import the Login component
 import SingleThread from "./components/forum/SingleThread.jsx";
 import Registration from "./components/registration/Registration.jsx";
 import MeetTheTeam from "./components/meet-the-team/MeetTheTeam.jsx";
-//import TopicsDropdown from "./components/forum/DropdownMenu/Dropdown/TopicsDropdown";
 import Events from "./components/events/Events.jsx";
 import Dashboard from "./components/dashboard/Dashboard.jsx";
 
--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -77,7 +77,7 @@ const Footer = () => {
           </div>
         </div>
 
-        {/* line break with horizonal line: below this copyright, etc. */}
+        {/* line break with horizontal line: below this copyright, Ts + Cs, etc. */}
         <hr></hr>
 
         <div className="footer-below">
